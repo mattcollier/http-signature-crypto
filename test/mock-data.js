f1e5d438707f9b642c121c7261a96a56d7e3508e
Matthew Collier <collier.matthew@gmail.com>
2018-05-10 12:17:54 -0400

    Add algorithms.

--- test/mock-data.js
+++ test/mock-data.js
@@ -12,3 +12,42 @@ mock.ed25519KeyPair = {
     '3Mmk4UzTRJTEtxaKk61LxtgUxAa2Dg36jF6VogPtRiKvfpsQWKPCLesKSV182RMmvM' +
     'JKk6QErH3wgdHp8itkSSiF'
 };
+
+mock.rsa2048KeyPair = {
+  publicKeyPem: '-----BEGIN PUBLIC KEY-----\n' +
+    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwzPDp8kvJlGHbQGHQGcp\n' +
+    'tO5iZG8iwED1QFNvJuKTZqYDoN44LUf95tYsjGqT38qj2/uo4zZRkfE3H1TEnsDo\n' +
+    'KbbRn1mqV3098sU/G9Kk8fsXL9eJrQ77sLoDQmZf0/huIqHw6/jN7m5p3bq80A0m\n' +
+    'gaJ56FuMq6IM4b9Sw40ajXTWQdiJqThN41eSHK01peT9jHMlnbQQwolqw0y9fkZ5\n' +
+    'oEGHezQH6+CVRXB2u7WveMWvow3+ssGDwoK6/YeSWUXFv0VZoQwVO0VmaIFcM11f\n' +
+    'G3KZD+iAayrF3xXz8ZPe0PY+6nZZi5/4HNy6B/30hAQn9X9I/0WMmmbQ5gCHJsu9\n' +
+    'WwIDAQAB\n' +
+    '-----END PUBLIC KEY-----\n',
+  privateKeyPem: '-----BEGIN RSA PRIVATE KEY-----\n' +
+    'MIIEpAIBAAKCAQEAwzPDp8kvJlGHbQGHQGcptO5iZG8iwED1QFNvJuKTZqYDoN44\n' +
+    'LUf95tYsjGqT38qj2/uo4zZRkfE3H1TEnsDoKbbRn1mqV3098sU/G9Kk8fsXL9eJ\n' +
+    'rQ77sLoDQmZf0/huIqHw6/jN7m5p3bq80A0mgaJ56FuMq6IM4b9Sw40ajXTWQdiJ\n' +
+    'qThN41eSHK01peT9jHMlnbQQwolqw0y9fkZ5oEGHezQH6+CVRXB2u7WveMWvow3+\n' +
+    'ssGDwoK6/YeSWUXFv0VZoQwVO0VmaIFcM11fG3KZD+iAayrF3xXz8ZPe0PY+6nZZ\n' +
+    'i5/4HNy6B/30hAQn9X9I/0WMmmbQ5gCHJsu9WwIDAQABAoIBAESCMWP8xvCC4q3O\n' +
+    'QILI8ilPFPc8zgx9f9XAsp0KHkODdniKJVs3DhRrDJ2Hdjiv7Qxy6ZY85Sn8Z6U2\n' +
+    'Yf95osGpKS7tEEy+ZvSCZ6DDMCLBRiUDV42GWa1vy18NgQprAXRkM6MN4nCRDdTF\n' +
+    'CilWxDHxLSnwn5FJQY4lUM3TAwOr5fBvSxjZiwDimykyy90wSqQxl1HI/badNipJ\n' +
+    'ZDCoVdIDFtYjjD03o7wsyOPFxD0sZnKnxSIS8kYeHMxK5Js20eLdHRocZdSebxbu\n' +
+    'bESUWaLg0sFn1tBg4y19hflAqzJGpta2wUombipkLm3DLJWQVMeq/52WJf7oqhr9\n' +
+    'Fg1BxkECgYEA4JQ6i+XSxMkZnsk0vCjEEQH+lfixV9+dQ9tBmn+FqNKTV40gix8+\n' +
+    'CMnB/ZKT6CknByQ1EojX7ZDj/c1qgoVYyRkcYcoQGzDwxZLydQpCncPT5/UYic0H\n' +
+    '8eKUCQLzwOnZo55mBmehxTqTwmtYjoSCWsGWoGO7ssEGSa+rIYd4SqECgYEA3oNX\n' +
+    'z53tE+Y8b7G95DEZIom32NpreMVq9T0xS8ZUYeDkR80a3Mli6IHz+qbFSd2ylcNf\n' +
+    'PbnJ1xeEGyGgMi9PG4E7GWTitl68uAOKnZ/83nHNtXQ5SJwRtxPiOyuh+HN8AAqX\n' +
+    'PfNCDffF6gAm+wfxyY1aAL04aBmzaaqEWpEGonsCgYEAxUqzHE+sl+ArN8l/IIWX\n' +
+    'qXFdHJc8BPyXhhNKUNYSr7s+Yb3DhzTNJJ9KYt+wPFZayPVQApZhS3zsLf2VwlAv\n' +
+    'LYt32ZjQCXM3MfrkMVnwJ/TvZml1Qynx/teUQU5soV9PKWRwMNQ906ygPj5br+hN\n' +
+    'NDm5f/Hd5S2ZvoYrCuueC8ECgYEArKTZ3/PXu6Xa5IrTHBdgOiUCqVWnJ1h9iXQG\n' +
+    'KJXkaOEWHgOswPvcKyyRQbxdvNcvtfWVkw3w5luPm4F2ixmb1mppkWVuZjORV3Ef\n' +
+    '/vbgOzOveQeJXqYBNLxPvrs2+8+WuW1+NYnliXLic5HUrNdYKZrr50DpYBP42ZZ9\n' +
+    'BMwbirsCgYA01XR+CS661Vy8bh6dEHa1IDoWBk3gqk9REUFnhdYLLEXy3zi1SnfI\n' +
+    'mXm+fICVKinK5jKA/iu0M1zLUY/rWjpIWQTMpk9AcLhWYwURPCkLw/E//T5iaw37\n' +
+    'cuRO/K57URWU78bQYpTMSCs0JhXnvHN1iCM5xyZ6rtz0jwZheWe+sA==\n' +
+    '-----END RSA PRIVATE KEY-----\n'
+};
--- test/test.js
+++ test/test.js
@@ -13,6 +13,7 @@ const hsc = require('..');
 describe('http-signature-crypto', () => {
 
   describe('Promise API', () => {
+
     describe('sign API', () => {
       it('returns an error on an unknown algorithm', () => {
         return hsc.sign({algorithm: 'abc'})
@@ -21,6 +22,7 @@ describe('http-signature-crypto', () => {
             err.message.should.equal('Unknown algorithm \'abc\'.');
           });
       });
+
       describe('Ed25519', () => {
         it('creates a signature', () => {
           const plaintext = 'abc123';
@@ -35,7 +37,94 @@ describe('http-signature-crypto', () => {
             });
         });
       });
+
+      describe('HMAC', () => {
+        it('creates a signature using sha256', () => {
+          const plaintext = 'abc123';
+          const sharedKey = 'secretKey';
+          return hsc.sign(
+            {algorithm: 'hmac', hashType: 'sha256', plaintext, sharedKey})
+            .then(signature => {
+              should.exist(signature);
+              signature.should.be.a('string');
+              signature.should.equal(
+                'FNAWrxLnd6YsyXGPvhZnoXvKERY+jzQ1TF2ucMmdEIA=');
+            });
+        });
+        it('creates a signature using sha512', () => {
+          const plaintext = 'abc123';
+          const sharedKey = 'secretKey';
+          return hsc.sign(
+            {algorithm: 'hmac', hashType: 'sha512', plaintext, sharedKey})
+            .then(signature => {
+              should.exist(signature);
+              signature.should.be.a('string');
+              signature.should.equal(
+                'mnTTyVH8P4ymXE3dl6DRAZFSic2ElBvLH5yqjJziFZSm1B3ZI' +
+                'V3r7KwKpbWasAZxPXjqZy8DnY8v++0+bVIQBQ==');
+            });
+        });
+        it('returns an error on invalid `hashType`', () => {
+          const plaintext = 'abc123';
+          const sharedKey = 'secretKey';
+          return hsc.sign(
+            {algorithm: 'hmac', hashType: 'unknown', plaintext, sharedKey})
+            .catch(err => {
+              should.exist(err);
+              err.message.should.contain('Unknown message digest');
+            });
+        });
+      });
+
+      describe('RSA', () => {
+        it('creates a signature using sha256', () => {
+          const plaintext = 'abc123';
+          const {privateKeyPem} = mockData.rsa2048KeyPair;
+          return hsc.sign(
+            {algorithm: 'rsa', hashType: 'sha256', plaintext, privateKeyPem})
+            .then(signature => {
+              should.exist(signature);
+              signature.should.be.a('string');
+              signature.should.equal(
+                'oHLS9K71TPOUQ7IG7Hdeq7jSmnoS6C2xyPvtlo56bizif1w/z' +
+                '62OEIiFpz2Oe784vDcWEI7/x5fYdB/uqIfdbkaquI78OZpVCl' +
+                'FVteTOmOw6EhQJ66hJLkml+8LnMSs0Y6v3Cd+wLsCfWpa/2PU' +
+                'ZcLjv1AOV3vhYhTdTXl4ufCwdWuSpPtA8L4LC7rak5mb/wmNCt' +
+                'iZ/Ol2/lhpwP6lPX+1UkH3EWFBTGDPnquk340TWRVXevrHDVSG' +
+                'F6Z8CS0TEmx1eX2+R8PJlRJ/ExA0vWEAc7O7Y7KctKKU+wWobL' +
+                'qftt9LGnThtWDKOHJlmQjt6k7S2WzvV5iIhVfLXee1tfw==');
+            });
+        });
+        it('creates a signature using sha512', () => {
+          const plaintext = 'abc123';
+          const {privateKeyPem} = mockData.rsa2048KeyPair;
+          return hsc.sign(
+            {algorithm: 'rsa', hashType: 'sha512', plaintext, privateKeyPem})
+            .then(signature => {
+              should.exist(signature);
+              signature.should.be.a('string');
+              signature.should.equal(
+                'Pkfeqzy5qKRROV6BmakMFpK+JFcbxRrHHbB9qHDiUMmM+sj66kSLMml/+' +
+                'T1lmCnWqXciupF1eG429j3Ulj706Ppog7AcvMSkSsKBy5WZ0yZCYqu6Fv' +
+                'FOdpWiRqNvNAA+7YB2WdSutt8ABxP23Fk0+mROq8bZ4UHw5WOS69LyokJ' +
+                'JwYt00HC8eK3MOuSdcjBrDAHh5EeoTN4hHTMlkpmY2SHYtjbyt9dIK2xX' +
+                'VYvWyUIdrYHpBs4gyFV0aHd/q1tNfzpL0m1/oSRKbHfx3RURqArBdeJi0' +
+                'N5CldSvFAMy4gT1elcsc5F3BRrEp1GkmzuwI+R80xWCVLi4Mbqn0xsADQ==');
+            });
+        });
+        it('returns an error on invalid `hashType`', () => {
+          const plaintext = 'abc123';
+          const {privateKeyPem} = mockData.rsa2048KeyPair;
+          return hsc.sign(
+            {algorithm: 'rsa', hashType: 'unknown', plaintext, privateKeyPem})
+            .catch(err => {
+              should.exist(err);
+              err.message.should.contain('Unknown message digest');
+            });
+        });
+      });
     });
+
     describe('verify API', () => {
       it('returns an error on an unknown algorithm', () => {
         return hsc.verify({algorithm: 'abc'})
@@ -44,6 +133,7 @@ describe('http-signature-crypto', () => {
             err.message.should.equal('Unknown algorithm \'abc\'.');
           });
       });
+
       describe('Ed25519', () => {
         it('verifies a signature', () => {
           const plaintext = 'abc123';
@@ -52,6 +142,43 @@ describe('http-signature-crypto', () => {
             algorithm: 'ed25519', plaintext, publicKeyBase58,
             signature: 'q/tQqxBlhzSP+XTte7uYaaCyJXJvg8svdjV47E2rBrVI1fBIOAeK' +
               'j5Jm7qBkH0IL8CvKRboqHBCoITDrsT9DAQ=='
+          }).then(verified => {
+            should.exist(verified);
+            verified.should.be.a('boolean');
+            verified.should.be.true;
+          });
+        });
+      });
+
+      describe('HMAC', () => {
+        it('verifies a signature', () => {
+          const plaintext = 'abc123';
+          const sharedKey = 'secretKey';
+          return hsc.verify({
+            algorithm: 'hmac', hashType: 'sha512', plaintext, sharedKey,
+            signature: 'mnTTyVH8P4ymXE3dl6DRAZFSic2ElBvLH5yqjJziFZSm1B3ZI' +
+            'V3r7KwKpbWasAZxPXjqZy8DnY8v++0+bVIQBQ=='})
+            .then(verified => {
+              should.exist(verified);
+              verified.should.be.a('boolean');
+              verified.should.be.true;
+            });
+        });
+      });
+
+      describe('RSA', () => {
+        it('verifies a signature', () => {
+          const plaintext = 'abc123';
+          const {publicKeyPem} = mockData.rsa2048KeyPair;
+          return hsc.verify({
+            algorithm: 'rsa', hashType: 'sha512', plaintext, publicKeyPem,
+            signature:
+              'Pkfeqzy5qKRROV6BmakMFpK+JFcbxRrHHbB9qHDiUMmM+sj66kSLMml/+' +
+              'T1lmCnWqXciupF1eG429j3Ulj706Ppog7AcvMSkSsKBy5WZ0yZCYqu6Fv' +
+              'FOdpWiRqNvNAA+7YB2WdSutt8ABxP23Fk0+mROq8bZ4UHw5WOS69LyokJ' +
+              'JwYt00HC8eK3MOuSdcjBrDAHh5EeoTN4hHTMlkpmY2SHYtjbyt9dIK2xX' +
+              'VYvWyUIdrYHpBs4gyFV0aHd/q1tNfzpL0m1/oSRKbHfx3RURqArBdeJi0' +
+              'N5CldSvFAMy4gT1elcsc5F3BRrEp1GkmzuwI+R80xWCVLi4Mbqn0xsADQ=='
           }).then(verified => {
             should.exist(verified);
             verified.should.be.a('boolean');
@@ -72,6 +199,7 @@ describe('http-signature-crypto', () => {
           done();
         });
       });
+
       describe('Ed25519', () => {
         it('creates a signature', done => {
           const plaintext = 'abc123';
@@ -89,7 +217,100 @@ describe('http-signature-crypto', () => {
             });
         });
       });
+
+      describe('HMAC', () => {
+        it('creates a signature using sha256', () => {
+          const plaintext = 'abc123';
+          const sharedKey = 'secretKey';
+          return hsc.sign(
+            {algorithm: 'hmac', hashType: 'sha256', plaintext, sharedKey},
+            (err, signature) => {
+              should.not.exist(err);
+              should.exist(signature);
+              signature.should.be.a('string');
+              signature.should.equal(
+                'FNAWrxLnd6YsyXGPvhZnoXvKERY+jzQ1TF2ucMmdEIA=');
+            });
+        });
+        it('creates a signature using sha512', () => {
+          const plaintext = 'abc123';
+          const sharedKey = 'secretKey';
+          return hsc.sign(
+            {algorithm: 'hmac', hashType: 'sha512', plaintext, sharedKey},
+            (err, signature) => {
+              should.not.exist(err);
+              should.exist(signature);
+              signature.should.be.a('string');
+              signature.should.equal(
+                'mnTTyVH8P4ymXE3dl6DRAZFSic2ElBvLH5yqjJziFZSm1B3ZI' +
+                'V3r7KwKpbWasAZxPXjqZy8DnY8v++0+bVIQBQ==');
+            });
+        });
+        it('returns an error on invalid `hashType`', () => {
+          const plaintext = 'abc123';
+          const sharedKey = 'secretKey';
+          return hsc.sign(
+            {algorithm: 'hmac', hashType: 'unknown', plaintext, sharedKey},
+            (err, signature) => {
+              should.exist(err);
+              should.not.exist(signature);
+              err.message.should.contain('Unknown message digest');
+            });
+        });
+      });
+
+      describe('RSA', () => {
+        it('creates a signature using sha256', () => {
+          const plaintext = 'abc123';
+          const {privateKeyPem} = mockData.rsa2048KeyPair;
+          return hsc.sign(
+            {algorithm: 'rsa', hashType: 'sha256', plaintext, privateKeyPem},
+            (err, signature) => {
+              should.not.exist(err);
+              should.exist(signature);
+              signature.should.be.a('string');
+              signature.should.equal(
+                'oHLS9K71TPOUQ7IG7Hdeq7jSmnoS6C2xyPvtlo56bizif1w/z' +
+                '62OEIiFpz2Oe784vDcWEI7/x5fYdB/uqIfdbkaquI78OZpVCl' +
+                'FVteTOmOw6EhQJ66hJLkml+8LnMSs0Y6v3Cd+wLsCfWpa/2PU' +
+                'ZcLjv1AOV3vhYhTdTXl4ufCwdWuSpPtA8L4LC7rak5mb/wmNCt' +
+                'iZ/Ol2/lhpwP6lPX+1UkH3EWFBTGDPnquk340TWRVXevrHDVSG' +
+                'F6Z8CS0TEmx1eX2+R8PJlRJ/ExA0vWEAc7O7Y7KctKKU+wWobL' +
+                'qftt9LGnThtWDKOHJlmQjt6k7S2WzvV5iIhVfLXee1tfw==');
+            });
+        });
+        it('creates a signature using sha512', () => {
+          const plaintext = 'abc123';
+          const {privateKeyPem} = mockData.rsa2048KeyPair;
+          return hsc.sign(
+            {algorithm: 'rsa', hashType: 'sha512', plaintext, privateKeyPem},
+            (err, signature) => {
+              should.not.exist(err);
+              should.exist(signature);
+              signature.should.be.a('string');
+              signature.should.equal(
+                'Pkfeqzy5qKRROV6BmakMFpK+JFcbxRrHHbB9qHDiUMmM+sj66kSLMml/+' +
+                'T1lmCnWqXciupF1eG429j3Ulj706Ppog7AcvMSkSsKBy5WZ0yZCYqu6Fv' +
+                'FOdpWiRqNvNAA+7YB2WdSutt8ABxP23Fk0+mROq8bZ4UHw5WOS69LyokJ' +
+                'JwYt00HC8eK3MOuSdcjBrDAHh5EeoTN4hHTMlkpmY2SHYtjbyt9dIK2xX' +
+                'VYvWyUIdrYHpBs4gyFV0aHd/q1tNfzpL0m1/oSRKbHfx3RURqArBdeJi0' +
+                'N5CldSvFAMy4gT1elcsc5F3BRrEp1GkmzuwI+R80xWCVLi4Mbqn0xsADQ==');
+            });
+        });
+        it('returns an error on invalid `hashType`', () => {
+          const plaintext = 'abc123';
+          const {privateKeyPem} = mockData.rsa2048KeyPair;
+          return hsc.sign(
+            {algorithm: 'rsa', hashType: 'unknown', plaintext, privateKeyPem},
+            (err, signature) => {
+              should.exist(err);
+              should.not.exist(signature);
+              err.message.should.contain('Unknown message digest');
+            });
+        });
+      });
     });
+
     describe('verify API', () => {
       it('returns an error on an unknown algorithm', done => {
         hsc.verify({algorithm: 'abc'}, (err, result) => {
@@ -97,6 +318,62 @@ describe('http-signature-crypto', () => {
           should.not.exist(result);
           err.message.should.equal('Unknown algorithm \'abc\'.');
           done();
+        });
+      });
+
+      describe('Ed25519', () => {
+        it('verifies a signature', () => {
+          const plaintext = 'abc123';
+          const {publicKeyBase58} = mockData.ed25519KeyPair;
+          return hsc.verify({
+            algorithm: 'ed25519', plaintext, publicKeyBase58,
+            signature: 'q/tQqxBlhzSP+XTte7uYaaCyJXJvg8svdjV47E2rBrVI1fBIOAeK' +
+              'j5Jm7qBkH0IL8CvKRboqHBCoITDrsT9DAQ=='
+          }, (err, verified) => {
+            should.not.exist(err);
+            should.exist(verified);
+            verified.should.be.a('boolean');
+            verified.should.be.true;
+          });
+        });
+      });
+
+      describe('HMAC', () => {
+        it('verifies a signature', () => {
+          const plaintext = 'abc123';
+          const sharedKey = 'secretKey';
+          return hsc.verify({
+            algorithm: 'hmac', hashType: 'sha512', plaintext, sharedKey,
+            signature: 'mnTTyVH8P4ymXE3dl6DRAZFSic2ElBvLH5yqjJziFZSm1B3ZI' +
+            'V3r7KwKpbWasAZxPXjqZy8DnY8v++0+bVIQBQ=='
+          }, (err, verified) => {
+            should.not.exist(err);
+            should.exist(verified);
+            verified.should.be.a('boolean');
+            verified.should.be.true;
+          });
+        });
+      });
+
+      describe('RSA', () => {
+        it('verifies a signature', () => {
+          const plaintext = 'abc123';
+          const {publicKeyPem} = mockData.rsa2048KeyPair;
+          return hsc.verify({
+            algorithm: 'rsa', hashType: 'sha512', plaintext, publicKeyPem,
+            signature:
+              'Pkfeqzy5qKRROV6BmakMFpK+JFcbxRrHHbB9qHDiUMmM+sj66kSLMml/+' +
+              'T1lmCnWqXciupF1eG429j3Ulj706Ppog7AcvMSkSsKBy5WZ0yZCYqu6Fv' +
+              'FOdpWiRqNvNAA+7YB2WdSutt8ABxP23Fk0+mROq8bZ4UHw5WOS69LyokJ' +
+              'JwYt00HC8eK3MOuSdcjBrDAHh5EeoTN4hHTMlkpmY2SHYtjbyt9dIK2xX' +
+              'VYvWyUIdrYHpBs4gyFV0aHd/q1tNfzpL0m1/oSRKbHfx3RURqArBdeJi0' +
+              'N5CldSvFAMy4gT1elcsc5F3BRrEp1GkmzuwI+R80xWCVLi4Mbqn0xsADQ=='
+          }, (err, verified) => {
+            should.not.exist(err);
+            should.exist(verified);
+            verified.should.be.a('boolean');
+            verified.should.be.true;
+          });
         });
       });
     });
